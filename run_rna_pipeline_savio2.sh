2bfc7699c0de6d95b98ffea4e99c986c261130c2
amaslan <annie@maslan.com>
2021-08-12 17:43:00 -0700

    add job script for savio2

---
+++ run_rna_pipeline_savio2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=amm_rna
+#
+# Account:
+#SBATCH --account=co_rosalind
+#
+# Partition:
+#SBATCH --partition=savio2_htc
+#
+# Quality of Service:
+#SBATCH --qos=rosalind_htc2_normal
+#
+# Wall clock limit:
+#SBATCH --time=2-00:00:00
+#
+# Nodes:
+#SBATCH --nodes=2
+# Specify number of tasks for use case (example):
+#SBATCH --ntasks-per-node=10
+## Command(s) to run:
+module load fastqc/0.11.9
+module load samtools/1.8
+module load multiqc/1.9
+snakemake -j --rerun-incomplete
