1e6b2d1579edaf5efa5471a2fdf0f620fd87c3ff
amaslan <annie@maslan.com>
2021-09-01 13:15:22 -0700

    rna-seq data download

---
+++ get_rna_ref_data.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=amm_rna
+#
+# Account:
+#SBATCH --account=co_rosalind
+#
+# Partition:
+#SBATCH --partition=savio
+#
+# Quality of Service:
+#SBATCH --qos=rosalind_savio_normal
+#
+# Wall clock limit:
+#SBATCH --time=2-00:00:00
+#
+# Nodes:
+#SBATCH --nodes=1
+## Command(s) to run:
+
+cd /clusterfs/rosalind/groups/streetslab/amaslan/ivana/data_ref
+
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590408/ERR590408_1.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590408/ERR590408_2.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590410/ERR590410_1.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590410/ERR590410_2.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590401/ERR590401_1.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590401/ERR590401_2.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590400/ERR590400_1.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590400/ERR590400_2.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590398/ERR590398_1.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590398/ERR590398_2.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590399/ERR590399_1.fastq.gz &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR590/ERR590399/ERR590399_2.fastq.gz
