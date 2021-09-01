20d11d0c4c3071d4105afa27dca6115402827dc5
amaslan <annie@maslan.com>
2021-09-01 13:18:41 -0700

    savio2

--- get_rna_ref_data.sh
+++ get_rna_ref_data.sh
@@ -6,16 +6,18 @@
 #SBATCH --account=co_rosalind
 #
 # Partition:
-#SBATCH --partition=savio
+#SBATCH --partition=savio2_htc
 #
 # Quality of Service:
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --qos=rosalind_htc2_normal
 #
 # Wall clock limit:
-#SBATCH --time=2-00:00:00
+#SBATCH --time=1-00:00:00
 #
 # Nodes:
 #SBATCH --nodes=1
+# Specify number of tasks for use case (example):
+#SBATCH --ntasks-per-node=10
 ## Command(s) to run:
 
 cd /clusterfs/rosalind/groups/streetslab/amaslan/ivana/data_ref
